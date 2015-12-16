ae198fc161fc204d1064bbe238261780bcf43286
Ram Shinde <ramshinde1992@gmail.com>
2015-12-16 13:31:46 +0530

    Changes for iterating over posts and users and adding async

--- code-section/public/javascripts/promises.js
+++ code-section/public/javascripts/promises.js
@@ -1,18 +1,22 @@
 $(function(){
   $.get('http://localhost:3000/posts/get-users')
     .then(function(users){
+      var ids = [];
       for(var index in users){
         $('.get-users').append('<li>'+JSON.stringify(users[index])+'</li>');
+        ids.push(getID(users[index].posts));
       }
 
-      return $.get('http://localhost:3000/posts/get-posts');
+      return getMultiple(ids, getPost);
     })
     .then(function(posts){
+      var ids = [];
       for(var index in posts){
         $('.get-posts').append('<li>'+JSON.stringify(posts[index])+'</li>');
+        ids.push(getID(posts[index].comments));
       }
 
-      return $.get('http://localhost:3000/posts/get-comments');
+      return getMultiple(ids, getComment);
     })
     .then(function(comments){
       for(var index in comments){
@@ -23,4 +27,32 @@ $(function(){
 
 function handleError(xhr, status, error){
   console.log(error);
+}
+
+function getPost(id){
+  return $.get('http://localhost:3000/posts/get-post/'+id).then(function(data, status, xhr){
+    return data;
+  });
+}
+
+function getComment(id){
+  return $.get('http://localhost:3000/posts/get-comment/'+id).then(function(data, status, xhr){
+    return data;
+  });
+}
+
+function getMultiple(ids, getReq){
+  var promises = ids.map(getReq);
+
+  return $.when.apply($, promises).then(function(){
+    return $.makeArray(arguments);
+  });
+}
+
+function getID(obj){
+  var id = "";
+  for(index in obj){
+    id = obj[index];
+  }
+  return id;
 }
