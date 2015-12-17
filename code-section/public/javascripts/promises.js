c2e1d47e3d6b1204bfc37b98c5e2e926741fbb18
Ram Shinde <ramshinde1992@gmail.com>
2015-12-17 15:56:35 +0530

    Changes for refactoring promises.js

--- code-section/public/javascripts/promises.js
+++ code-section/public/javascripts/promises.js
@@ -1,12 +1,7 @@
 $(function(){
   $.get('http://localhost:3000/posts/get-users')
     .then(function(users){
-      for(var index in users){
-        var a = users[index];
-        $('.get-users').append(`<li>
-          <a data-uid="${a._id}" class="user">${a.name}</a>
-        </li>`);
-      }
+      insertUsers(users);
     });
 
   var getUser = function(uid){
@@ -21,54 +16,78 @@ $(function(){
     var uid = $(this).data('uid');
       getUser(uid)
       .then(function(user){
-        var promises = [];
-
-        $('.user-data').html(`
-          <p> Name: ${user.name} </p>
-          <p> Username: ${user.username} </p>
-        `);
-
-        user.posts.forEach(function(val, key){
-          var p = $.get('http://localhost:3000/posts/get-post/'+ val);
-          promises.push(p);
-        });
-        return Promise.all(promises);
+        var postPromises = insertUserDetail(user);
+        return Promise.all(postPromises);
       })
       .then(function(posts){
-        var postsHTML = `<p> Posts of User </p><ul>`;
-        var commentPromise = {};
-
-        posts.forEach(function(val, key){
-          var promises = [];
-          postsHTML += `<li class="${val._id}">
-            <p>Tag: ${val.title},  Description: ${val.description}</p>
-            <p>Comments: </p><ul class= "comment-section"></ul>
-          </li>`;
-
-          val.comments.forEach(function(val, key){
-            var p = $.get('http://localhost:3000/posts/get-comment/'+ val);
-            promises.push(p);
-          });
-          commentPromise[val._id] = Promise.all(promises);
-        });
-        postsHTML += '</ul>';
-
-        $('.user-post').html(postsHTML);
-        return Promise.props(commentPromise);
+        var commentPromises = insertUserPost(posts);
+        return Promise.props(commentPromises);
       })
       .then(function(comments){
-        Object.keys(comments).forEach(function(val, key){
-          var commentsHTML = ``;
-          comments[val].forEach(function(value, key){
-            commentsHTML += `<li>${value.description}</li>`;
-          });
-          $('.'+val).find('.comment-section').append(commentsHTML);
-        });
-
+        insertComment(comments);
       });
   });
 });
 
 function handleError(xhr, status, error){
   console.log(error);
+}
+
+function insertUsers(users){
+  for(var index in users){
+    var a = users[index];
+    $('.get-users').append(`<li>
+      <a data-uid="${a._id}" class="user">${a.name}</a>
+    </li>`);
+  }
+}
+
+function insertUserDetail(user){
+  var promises = [];
+  console.log("Inside user details");
+  $('.user-data').html(`
+    <p> Name: ${user.name} </p>
+    <p> Username: ${user.username} </p>
+  `);
+
+  user.posts.forEach(function(val, key){
+    var p = $.get('http://localhost:3000/posts/get-post/'+ val);
+    promises.push(p);
+  });
+
+  return promises;
+}
+
+function insertUserPost(posts){
+  var postsHTML = `<p> Posts of User </p><ul>`;
+  var commentPromise = {};
+
+  posts.forEach(function(val, key){
+    var promises = [];
+    postsHTML += `<li class="${val._id}">
+      <p>Tag: ${val.title},  Description: ${val.description}</p>
+      <p>Comments: </p><ul class= "comment-section"></ul>
+    </li>`;
+
+    val.comments.forEach(function(val, key){
+      var p = $.get('http://localhost:3000/posts/get-comment/'+ val);
+      promises.push(p);
+    });
+    commentPromise[val._id] = Promise.all(promises);
+  });
+  postsHTML += '</ul>';
+
+  $('.user-post').html(postsHTML);
+
+  return commentPromise;
+}
+
+function insertComment(comments){
+  Object.keys(comments).forEach(function(val, key){
+    var commentsHTML = ``;
+    comments[val].forEach(function(value, key){
+      commentsHTML += `<li>${value.description}</li>`;
+    });
+    $('.'+val).find('.comment-section').append(commentsHTML);
+  });
 }
